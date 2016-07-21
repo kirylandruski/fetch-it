26836492e1654cd63e3972b85116c6bb4adf930e
kirylandruski <kiryl.andruski@gmail.com>
2016-07-21 18:27:33 +0300

    Updated middleware logic
    Array has to be cloned before being reversed.

--- src/fetch-it.js
+++ src/fetch-it.js
@@ -56,7 +56,7 @@ class FetchIt {
     let promise = global.Promise.resolve(request);
     let chain = [global.fetch, undefined];
 
-    for (let middleware of this.middlewares.reverse()) {
+    for (let middleware of this.middlewares.slice(0).reverse()) {
       chain.unshift(middleware.request, middleware.requestError);
       chain.push(middleware.response, middleware.responseError);
     }
